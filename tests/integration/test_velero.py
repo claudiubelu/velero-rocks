dc660eef5ac13ce9bbea8403993feba532677c08
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-26 15:38:18 +0300

    Tests all the velero versions (#13)
    Adds integration tests for all the currently supported velero
    versions.

--- tests/integration/test_velero.py
+++ tests/integration/test_velero.py
@@ -5,8 +5,10 @@ import logging
 import uuid
 from pathlib import Path
 
+import pytest
 from k8s_test_harness import harness
 from k8s_test_harness.util import env_util, k8s_util
+from packaging import version
 
 LOG = logging.getLogger(__name__)
 
@@ -14,9 +16,17 @@ DIR = Path(__file__).absolute().parent
 MANIFESTS_DIR = DIR / ".." / "templates"
 
 
-def _get_velero_helm_cmd():
+VELERO_CHART_VERSIONS = [
+    # (velero_version, chart_version)
+    ("1.13.2", "6.7.0"),
+    ("1.12.1", "5.2.2"),
+    ("1.9.5", "2.32.6"),
+]
+
+
+def _get_velero_helm_cmd(velero_version, chart_version):
     velero_rock = env_util.get_build_meta_info_for_rock_version(
-        "velero", "1.13.2", "amd64"
+        "velero", velero_version, "amd64"
     )
     kubectl_rock = env_util.get_build_meta_info_for_rock_version(
         "kubectl", "1.30.2", "amd64"
@@ -33,13 +43,21 @@ aws_secret_access_key = minio123
 """
     minio_url = "http://minio.velero.svc:9000"
 
+    # Velero chart version 4.0.0 introduced multiple backup storage locations.
+    # Previously, the configuration.provider configuration was different.
+    index = ""
+    provider = "provider"
+    if version.Version(chart_version) >= version.Version("4.0.0"):
+        index = "[0]"
+        provider = "backupStorageLocation[0].provider"
+
     set_configs = [
         f"credentials.secretContents.cloud={credentials}",
-        "configuration.backupStorageLocation[0].provider=aws",
-        "configuration.backupStorageLocation[0].bucket=velero",
-        "configuration.backupStorageLocation[0].config.region=minio",
-        "configuration.backupStorageLocation[0].config.s3ForcePathStyle=true",
-        f"configuration.backupStorageLocation[0].config.s3Url={minio_url}",
+        f"configuration.{provider}=aws",
+        f"configuration.backupStorageLocation{index}.bucket=velero",
+        f"configuration.backupStorageLocation{index}.config.region=minio",
+        f"configuration.backupStorageLocation{index}.config.s3ForcePathStyle=true",
+        f"configuration.backupStorageLocation{index}.config.s3Url={minio_url}",
         "snapshotsEnabled=false",
         "initContainers[0].name=velero-plugin-for-aws",
         "initContainers[0].image=velero/velero-plugin-for-aws:v1.2.1",
@@ -54,6 +72,7 @@ aws_secret_access_key = minio123
         repository="https://vmware-tanzu.github.io/helm-charts",
         images=images,
         set_configs=set_configs,
+        chart_version=chart_version,
     )
 
 
@@ -74,34 +93,37 @@ def _exec_velero_cmd(instance, deployment_name, namespace, *cmd):
     )
 
 
-def test_integration_velero(module_instance: harness.Instance):
+@pytest.mark.parametrize("velero_version,chart_version", VELERO_CHART_VERSIONS)
+def test_integration_velero(
+    function_instance: harness.Instance, velero_version, chart_version
+):
     # Setup Minio first, which is an S3-compatible storage service. We'll use
     # it to test out Velero's functionality with it.
     manifest = MANIFESTS_DIR / "minio-deployment.yaml"
-    module_instance.exec(
+    function_instance.exec(
         ["k8s", "kubectl", "apply", "-f", "-"],
         input=Path(manifest).read_bytes(),
     )
 
-    k8s_util.wait_for_deployment(module_instance, "minio", "velero")
+    k8s_util.wait_for_deployment(function_instance, "minio", "velero")
 
     # Deploy Velero rock.
-    module_instance.exec(_get_velero_helm_cmd())
-    k8s_util.wait_for_deployment(module_instance, "velero", "velero")
+    function_instance.exec(_get_velero_helm_cmd(velero_version, chart_version))
+    k8s_util.wait_for_deployment(function_instance, "velero", "velero")
 
     # Deploy an nginx app which we'll back up.
     manifest = MANIFESTS_DIR / "nginx-deployment.yaml"
-    module_instance.exec(
+    function_instance.exec(
         ["k8s", "kubectl", "apply", "-f", "-"],
         input=Path(manifest).read_bytes(),
     )
 
-    k8s_util.wait_for_deployment(module_instance, "nginx-deployment", "nginx-example")
+    k8s_util.wait_for_deployment(function_instance, "nginx-deployment", "nginx-example")
 
     # Back the nginx app.
     backup_name = f"nginx-backup-{uuid.uuid4()}"
     _exec_velero_cmd(
-        module_instance,
+        function_instance,
         "velero",
         "velero",
         "backup",
@@ -112,7 +134,7 @@ def test_integration_velero(module_instance: harness.Instance):
     )
 
     # Delete the nginx app, we should be able to restore it.
-    module_instance.exec(
+    function_instance.exec(
         [
             "k8s",
             "kubectl",
@@ -127,7 +149,7 @@ def test_integration_velero(module_instance: harness.Instance):
 
     # Restore it, and expect it to become available.
     _exec_velero_cmd(
-        module_instance,
+        function_instance,
         "velero",
         "velero",
         "restore",
@@ -136,4 +158,4 @@ def test_integration_velero(module_instance: harness.Instance):
         backup_name,
     )
 
-    k8s_util.wait_for_deployment(module_instance, "nginx-deployment", "nginx-example")
+    k8s_util.wait_for_deployment(function_instance, "nginx-deployment", "nginx-example")
